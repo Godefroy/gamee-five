809eb44468fb22833b7f41643d658f765de05e1c
Godefroy <git@skreo.net>
2015-09-11 17:04:34 +0200

    Added usage of settings.server.host in Show page

--- web/routes.js
+++ web/routes.js
@@ -1,15 +1,18 @@
 var router = require("express").Router();
+var settings = require('../settings');
 
 router.get("/", function(req, res, next) {
-	res.redirect("/play");
+  res.redirect("/play");
 });
 
 router.get("/play", function(req, res, next) {
-	res.render("play");
+  res.render("play");
 });
 
 router.get("/show", function(req, res, next) {
-	res.render("show");
+  res.render("show", {
+    host: settings.server.host
+  });
 });
 
 module.exports = router;
